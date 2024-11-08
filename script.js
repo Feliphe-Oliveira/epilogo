7b946ac5c47987a484b9af05efd9f94d6d7bc12b
Feliphe-Oliveira <oliveirafeliphe272@gmail.com>
2024-11-07 21:46:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('btnEpi').addEventListener('click', ()=>{
-    const cod = 1221
-    if(document.getElementById('senha').value == cod){
+    const cod = 'importante'
+    if(document.getElementById('senha').value.toUpperCase() == cod.toUpperCase()){
         document.getElementById('cx_verso').classList.add('destaque')
         document.getElementById('login').classList.add('semdestaque')
     }else if(document.getElementById('senha').value == ''){
